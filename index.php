bdd3ba0f70f7d9935c8e3f275d5353e604bf4f8b
Serban Paun <61983199+serbanpaun@users.noreply.github.com>
2025-11-22 00:50:21 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Strong Password Generator • PTM.ro</title>
+<meta name="description" content="Strong Password Generator. We *DO NOT* store anything on our servers. Your data is secure!">
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<a href="/">
+  <span class="title"><strong>Strong</strong> Password Generator</span>
+</a>
+
+<form id="pwform" class="form-horizontal">
+<fieldset>
+<div id="options" onchange="switchbtn();" onfocus="switchbtn();">
+  <input type="checkbox" checked name="alpha" id="lowercase"><label for="lowercase">lowercase</label><br>
+  <input type="checkbox" checked name="alpha_upper" id="uppercase"><label for="uppercase">UPPERCASE</label><br>
+  <input type="checkbox" checked name="numeric" id="numeric"><label for="numeric">Numb3r5</label><br>
+  <input type="checkbox" checked name="special" id="special"><label for="special">$ymbols</label>
+  <input type="checkbox" name="lowspecial" id="lowspecial"><label for="lowspecial">Limited</label><br>
+
+  <button type="button" class="btn btn-primary" onclick="toggle();">Toggle</button>
+  <button type="button" class="btn btn-primary" onclick="selectall();">Select all</button>
+  <hr>
+  <div id="bottom">
+  <button id="submit" type="submit" class="btn btn-primary">Generate</button> <input id="length" name="length" type="number" min=16 max=128 value=16> characters
+
+	<script>
+  const num = document.getElementById('length');
+	num.addEventListener('wheel', function(e) {
+	  e.preventDefault();
+	  const step = parseFloat(this.step) || 1;
+	  let value = parseFloat(this.value) || 0;
+
+	  if (e.deltaY < 0) value += step;
+	  else value -= step;
+
+	  if (this.hasAttribute('min')) value = Math.max(value, parseFloat(this.min));
+	  if (this.hasAttribute('max')) value = Math.min(value, parseFloat(this.max));
+
+	  this.value = value;
+	});
+	</script>
+  <div id="notice">
+  	Minimum password length is 16 characters!<br>
+  	We do not generate passwords longer than 128 characters, because that would be madness!
+  </div>
+  </div>
+</div>
+</fieldset>
+</form>
+
+<script>
+const pwlower = document.getElementById('lowercase');
+const pwupper = document.getElementById('uppercase');
+const pwnumer = document.getElementById('numeric');
+const pwspecs = document.getElementById('special');
+const pwlowspecs = document.getElementById('lowspecial');
+const generatebtn = document.getElementById('submit');
+
+function switchbtn() {
+  generatebtn.disabled = !pwlower.checked && !pwupper.checked && !pwnumer.checked && !pwspecs.checked && !pwlowspecs.checked;
+}
+
+function selectall() {
+  pwlower.checked = true;
+  pwupper.checked = true;
+  pwnumer.checked = true;
+  pwspecs.checked = true;
+  pwlowspecs.checked = false;
+  switchbtn();
+}
+function toggle() {
+  pwlower.checked = !pwlower.checked;
+  pwupper.checked = !pwupper.checked;
+  pwnumer.checked = !pwnumer.checked;
+  pwspecs.checked = !pwspecs.checked;
+  pwlowspecs.checked = !pwlowspecs.checked;
+  switchbtn();
+}
+
+document.getElementById('pwform').addEventListener('submit', function(e) {
+  e.preventDefault(); // stop page refresh
+  generatePassword();
+});
+
+function getRandomChar(str) {
+  const array = new Uint32Array(1);
+  window.crypto.getRandomValues(array);
+  return str[array[0] % str.length];
+}
+
+function generatePassword() {
+  const alpha = "abcdefghjkmnpqrstuvwxyz";
+  const alpha_upper = alpha.toUpperCase();
+  const numeric = "0123456789";
+  const lowspecial = "!@#$%&*-=+_";
+  const special = lowspecial + "^[]'\\{}|,./?<>";
+  let chars = "";
+  let required = [];
+
+  if (pwlower.checked) {
+    chars += alpha;
+    required.push(getRandomChar(alpha));
+  }
+  if (pwupper.checked) {
+    chars += alpha_upper;
+    required.push(getRandomChar(alpha_upper));
+  }
+  if (pwnumer.checked) {
+    chars += numeric;
+    required.push(getRandomChar(numeric));
+  }
+  if (pwlowspecs.checked) {
+    chars += lowspecial;
+    required.push(getRandomChar(lowspecial));
+  } else if (pwspecs.checked) {
+    chars += special;
+    required.push(getRandomChar(special));
+  }
+
+  const len = parseInt(document.getElementById('length').value);
+  let pw = required;
+
+  for (let i = required.length; i < len; i++) {
+    pw.push(getRandomChar(chars));
+  }
+
+  for (let i = pw.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [pw[i], pw[j]] = [pw[j], pw[i]];
+  }
+
+  document.getElementById('cptctxt').innerText = pw.join('');
+  document.getElementById('pwdbox').style.display = 'block';
+}
+</script>
+
+<script>
+function copyToClipboard() {
+  const content = document.getElementById('cptctxt');
+  navigator.clipboard.writeText(content.innerText);
+    const hint = document.getElementById('hint');
+    hint.style.display = "block";
+    hint.style.opacity = 1;
+    setTimeout(() => {
+      hint.style.opacity = 0;
+      hint.addEventListener('transitionend', function handler() {
+        hint.style.display = "none";
+        hint.removeEventListener('transitionend', handler);
+      });
+    }, 2000);
+}
+</script>
+
+<div id="pwdbox">
+  <div id="hint">Copied to clipboard</div>
+  <pre id="cptctxt" onclick="copyToClipboard()"></pre>
+</div>
+<footer>
+<small>No generated passwords are stored on this server. &bull; <span class="red">PTM.ro</span> &bull; 2015 - <script>document.write(new Date().getFullYear())</script> &copy; All rights reserved &bull; 
+  This code is
+  <a href="https://validator.w3.org/nu/?doc=https%3A%2F%2Fpasswd.ptm.ro%2F" title="W3C Markup Validation">HTML5</a>
+   and
+  <a href="https://jigsaw.w3.org/css-validator/validator?uri=https%3A%2F%2Fpasswd.ptm.ro&profile=css3svg&usermedium=all&warning=1&vextwarning=&lang=en" title="W3C CSS Validation Service">CSS3</a> valid.
+</small>
+</footer>
+</body>
+</html>
